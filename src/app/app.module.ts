719b11352d40f2d25430cd2b348fdb71700b5a54
Jonathan Guerrera <jrrera@gmail.com>
2018-08-06 16:26:32 -0700

    Add surveyjs with config on helloWorld page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, ErrorHandler } from '@angular/core';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, ErrorHandler } from "@angular/core";
+import { IonicApp, IonicModule, IonicErrorHandler } from "ionic-angular";
+import { MyApp } from "./app.component";
 
-import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
-import { ItemDetailsPage } from '../pages/item-details/item-details';
-import { ListPage } from '../pages/list/list';
+import { HelloIonicPage } from "../pages/hello-ionic/hello-ionic";
+import { ItemDetailsPage } from "../pages/item-details/item-details";
+import { ListPage } from "../pages/list/list";
+import { SurveyComponent } from "./survey.component";
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from "@ionic-native/status-bar";
+import { SplashScreen } from "@ionic-native/splash-screen";
 
 @NgModule({
   declarations: [
     MyApp,
     HelloIonicPage,
     ItemDetailsPage,
-    ListPage
+    ListPage,
+    SurveyComponent
   ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp),
-  ],
+  imports: [BrowserModule, IonicModule.forRoot(MyApp)],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HelloIonicPage,
-    ItemDetailsPage,
-    ListPage
-  ],
+  entryComponents: [MyApp, HelloIonicPage, ItemDetailsPage, ListPage],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
 export class AppModule {}
--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,11 +1,56 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+// import { SurveyComponent } from "../../app/survey.component";
 
 @Component({
-  selector: 'page-hello-ionic',
-  templateUrl: 'hello-ionic.html'
+  selector: "page-hello-ionic",
+  templateUrl: "hello-ionic.html"
 })
 export class HelloIonicPage {
   constructor() {
-
+    this.surveyConfig = {
+      questions: [
+        {
+          name: "numCigarettes",
+          type: "text",
+          title: "How many cigarettes have you smoked since your last survey?",
+          placeHolder: "0",
+          isRequired: true,
+          inputType: "tel",
+          validators: [{ type: "numeric", minValue: 0, maxValue: 200 }]
+        },
+        {
+          name: "feelDepressed",
+          type: "bootstrapslider",
+          title: "Right now, I feel down or depressed. (replace with slider)",
+          isRequired: true
+        },
+        {
+          name: "feelHappy",
+          type: "nouislider",
+          title: "Right now, I feel happy.",
+          isRequired: true
+        },
+        {
+          name: "craveCigarette",
+          type: "nouislider",
+          title: "Right now, I am craving a cigarette.",
+          isRequired: true
+        },
+        {
+          name: "currentLocation",
+          type: "text",
+          inputType: "text",
+          title: "Please describe your current location.",
+          isRequired: true
+        },
+        {
+          name: "allowedSmoking",
+          type: "radiogroup",
+          title: "Are you in a location that allows smoking?",
+          isRequired: true,
+          choices: ["Yes", "No"]
+        }
+      ]
+    };
   }
 }
