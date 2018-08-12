972e740ab7e6864abd79d17f709ab67d5b572ebf
Jonathan Guerrera <jrrera@gmail.com>
2018-08-12 11:39:13 -0700

    Improve styles, remove unused code

--- src/app/survey.component.ts
+++ src/app/survey.component.ts
@@ -2,7 +2,6 @@ import { Component, Input } from "@angular/core";
 import * as Survey from "survey-angular";
 
 import noUiSlider from "nouislider";
-// import "nouislider/distribute/nouislider.min.css";
 import * as widgets from "surveyjs-widgets";
 
 @Component({
@@ -25,30 +24,10 @@ export class SurveyComponent {
     Survey.SurveyNG.render("surveyElement", { model: surveyModel });
 
     surveyModel.onValueChanged.add((survey, options) => {
-      const inputElem = document.getElementById("test-area");
-      const BOOTSTRAP_SLIDER = "bootstrapslider"; 
-      inputElem.textContent = '';
-
-      console.log(options);
-      
-      if (options.question.customTypeName === BOOTSTRAP_SLIDER) {
-        let slider = options.question.bootstrapSlider;
-        console.log('template', slider);
-        
-        inputElem.innerText = options.value;
-
-        slider.setAttribute('ticks', [0, 100])
-        slider.setAttribute('ticks_positions', [0, 100])
-        slider.setAttribute('ticks_labels', ['first', 'last'])
-        slider.setAttribute('tooltip', 'show')
-        // slider.setAttribute('tooltip_position', 'bottom')
-        console.log('refreshing...');
-        slider.refresh();
-        
-      } 
-
       if (options.question.customTypeName === 'nouislider') {
         
+        // Use setTimeout because there was a DOM rendering race condition causing 
+        // this to fail sometimes.
         setTimeout(function() {
           let slider = options.question.noUiSlider;
           console.log(slider);
@@ -59,26 +38,12 @@ export class SurveyComponent {
             values: [0, 100],
             format: {
               to: function (a) {
-                return a === 0 ? '0 (not at all)' : '100 (as much as possible)'
+                return a === 0 ? '0 (not at all)' : '(as much as possible) 100'
               }
             }
           })
         }, 100);
       }
-      // ADD OVERRIDES HERE TO CUSTOMIZE BEHAVIOR OF THE SLIDER OF CHOICE.
-      // if(options.name !== "know") return;
-      // knownChoices = options.question.choices;
-      // var choices = [];
-      // for(var i = 0; i < knownChoices.length; i ++) {
-      //     var item = knownChoices[i];
-      //     //the item is not selected
-      //     if(options.value.indexOf(item.value) < 0) {
-      //         choices.push(item);
-      //     }
-      // }
-      // var learnQuestion = survey.getQuestionByName("learn");
-      // learnQuestion.choices = choices;
-      // learnQuestion.visible = choices.length > 0;
     });
   }
 }
